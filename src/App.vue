e24b5d7572bac20b700b8f717a7913ac4499c039
Yuna <yuna9068@gmail.com>
2021-02-16 13:14:22 +0800

    feat: 串接農場資料 API

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'App',
+  created() {
+    this.getFarm();
+  },
+  methods: {
+    ...mapActions(['saveFarm']),
+    /**
+     * 取得所有農場資料
+     */
+    getFarm() {
+      const apiFarm = 'http://192.168.43.19:3000/api/farm-stay';
+      $.ajax({
+        method: 'GET',
+        url: apiFarm,
+      }).done((response) => {
+        this.saveFarm(response);
+      }).fail((response) => {
+        console.log('fail: ', response);
+      }).always(() => {
+        console.log('hide loading');
+      });
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    farm: [], // 所有農場資料
   },
   mutations: {
+    FARM(state, payload) {
+      state.farm = payload;
+    },
   },
   actions: {
+    saveFarm(context, payload) {
+      context.commit('FARM', payload);
+    },
   },
   modules: {
   },
