0301d30a37b03e661977047b9e0fa48695e3a34e
Yuna <yuna9068@gmail.com>
2021-02-17 21:52:32 +0800

    feat: 新增回到上方與移到底部按鈕

---
+++ src/components/ScrollButton.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="scrollButton position-fixed d-flex">
+    <button
+      type="button"
+      class="btn text-info border-0 rounded-circle font-weight-bolder mr-3"
+      @click="toTop()"
+    >
+      &#9650;
+    </button>
+    <button
+      type="button"
+      class="btn text-info border-0 rounded-circle font-weight-bolder"
+      @click="toBottom()"
+    >
+      &#9660;
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrollButton',
+  mounted() {
+    this.scrollEvent();
+  },
+  methods: {
+    scrollEvent() {
+      const navbarHeight = $('#navbarMain').innerHeight();
+      $(window).scroll(() => {
+        if ($(window).scrollTop() > navbarHeight) {
+          $('.scrollButton').addClass('enter');
+        } else {
+          $('.scrollButton').removeClass('enter');
+        }
+      });
+    },
+    toTop() {
+      $('html, body').animate({
+        scrollTop: 0,
+      }, 1000, 'linear');
+    },
+    toBottom() {
+      $('html, body').animate({
+        scrollTop: document.body.scrollHeight,
+      }, 1000, 'linear');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$transition-time: 0.4s;
+
+.scrollButton {
+  right: -100px;
+  bottom: 20px;
+  opacity: 0;
+  transition: all $transition-time linear;
+  .btn {
+    background-color: rgba($light, 0.8);
+    opacity: 0;
+    transform: rotate(360deg);
+    transition: all $transition-time linear;
+    &:focus {
+      box-shadow: none;
+    }
+  }
+  &.enter {
+    opacity: 1;
+    right: 20px;
+    transition: all $transition-time linear;
+    .btn {
+      opacity: 1;
+      transform: rotate(0deg);
+      transition: all $transition-time linear;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <NavRegion/>
     <FarmList/>
+    <ScrollButton/>
     <p>查詢農場名稱： {{ getSearchCondition.keyword }}</p>
     <p>查詢農場縣市： {{ getSearchCondition.county }}</p>
     <p>查詢農場鄉鎮： {{ getSearchCondition.town }}</p>
@@ -13,12 +14,14 @@
 import { mapGetters } from 'vuex';
 import NavRegion from '@/components/NavRegion.vue';
 import FarmList from '@/components/FarmList.vue';
+import ScrollButton from '@/components/ScrollButton.vue';
 
 export default {
   name: 'Home',
   components: {
     NavRegion,
     FarmList,
+    ScrollButton,
   },
   computed: {
     ...mapGetters(['getSearchCondition', 'getFilterFarm']),
